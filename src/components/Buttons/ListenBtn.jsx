c543c2a1ecd93ef1f2088fe44992297c4a71247b
Rahul Yavvari <rahulyavvari04@gmail.com>
2024-06-03 15:25:05 +0530

    Clickable ListenBtn with MaxDuration Functionality

--- src/components/Buttons/ListenBtn.jsx
+++ src/components/Buttons/ListenBtn.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
 
 function ListenBtn() {
+
+    const waitTime = 6000;
+    const [isActive, setIsActive] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+    const hasPageBeenRendered = useRef(false);
+
+    let timer = null;
+
+    useEffect(() => {
+        if (hasPageBeenRendered.current) {
+            timer = setTimeout(() => {
+                if (isClicked) {
+                    const pulsatingCircle = document.getElementsByClassName("pulsating-circle")[0];
+                    pulsatingCircle.classList.toggle("pulsating-circle-animation");
+                }
+                setIsClicked(false);
+            }, waitTime);
+        }
+        
+        hasPageBeenRendered.current = true;
+
+    }, [isActive]);
+
+    const recordAudio = () => {
+            // DO SOMETHING
+            setIsActive((prevState) => ({isActive: !prevState}));
+    }
+
+    const handleClick = () => {
+        const pulsatingCircle = document.getElementsByClassName("pulsating-circle")[0];
+        pulsatingCircle.classList.toggle("pulsating-circle-animation");
+        if (isClicked) {
+            setIsClicked(false);
+            clearTimeout(timer);
+        } else {
+            setIsClicked(true);
+            recordAudio();
+        }
+    }
+
     return (
-        <div className="circle-container">
+        <div className="circle-container" onClick={handleClick}>
             <div className="fixed-circle">Listen</div>
             <div className="pulsating-circle"></div>
         </div>
